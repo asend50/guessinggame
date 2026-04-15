211162b120778783377c9ae1558ee692e30ca3e2
asend50 <asend50@kprschools.ca>
2026-04-15 08:44:05 -0400

    guessinggame

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,24 @@
 /*
 By: <Asen Doiron>
 Date: 2026-04-08
-Program Details: <The purpose of this program is to allow the user to guess a number from 1 to 100 and see if they are correct.>
+Program Details: <The purpose of this program is to allow the user to guess a number from 1 to 100 until they guess the correct number, then the program will ask if the user wants to play again>
 */
 
 use rand::Rng;
 use std::io;
 
 fn main() {
-    let mut rng = rand::rng(); // Generate a random value in the given range.
-    let random_int: i32 = rng.random_range(1..=100); // Generate a random value between 1 and 100
-    println!("Welcome to the guessing game that picks a random number between 1 and 100, so you can guess it until you get it right!");
-loop {
-    let mut input = String::new();
-    println!("Guess a number from 1 to 100:");
-    io::stdin().read_line(&mut input).expect("Failed to read line");
-    let number: i32 = input.trim().parse().expect("Please type a number!");
+    let mut rng = rand::rng();
+    let mut random_int: i32 = rng.random_range(1..=100);
+    println!("Type a number between 1 and 100:");
 
-    
+    loop {
+        let mut input = String::new();
+        io::stdin().read_line(&mut input).expect("Failed to read line");
 
+        let number: i32 = input.trim().parse().expect("Please type a number!");
         let diff = (number as i32 - random_int as i32).abs();
+
         if diff >= 50 {
             println!("FREEING, your guess was:{}", number);
         } else if diff >= 25 {
@@ -30,12 +29,25 @@ loop {
             println!("WARM, your guess was:{}", number);
         } else if diff >= 5 {
             println!("HOT, your guess was:{}", number);
-        }else if diff > 0 {
+        } else if diff > 0 {
             println!("BOILING, your guess was:{}", number);
-        }if number == random_int {
-        println!("You guessed correctly! The number was: {}", random_int);
-        break;
-    } else {
-        }}
-    }
+        } else if diff == 0 {
+            println!("You guessed correctly! The number was: {}", number);
+            println!("Would you like to play again? (y/n)");
 
+            let mut newgame = String::new();
+            io::stdin().read_line(&mut newgame).expect("Failed to read line");
+
+            let newgame = newgame.trim();
+            if newgame.trim() == "y" || newgame.trim() == "Y" {
+                random_int = rng.random_range(1..=100);
+                println!("Type a number between 1 and 100:");
+            }
+
+            if newgame.trim() == "n" || newgame.trim() == "N" {
+                println!("Thanks For Playing!");
+                break;
+            }
+        }
+    }
+}
